4692a3571856d8dec23b7d457dc726c08b9c9cf6
ThomasPalamara <toma06pal@gmail.com>
2018-04-16 00:10:07 +0100

    WebPack end

--- src/app.js
+++ src/app.js
@@ -1,42 +1,6 @@
-    console.log('App.js is running!');
+import React from "react";
+import ReactDOM from "react-dom";
 
-    const app = {
-        title: 'Indecision app',
-        subtitle: 'Put your life in the hands of a computer',
-        options: ['One','Two']
-    }
-    //JSX - JavaScript XML
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options.length>0 ? 'Here are your options :' : 'No options'}</p>
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </ul>
-        </div>
-    );
+import IndecisionApp from "./components/IndecisionApp.js";
 
-    const user ={
-        name: 'Thomas',
-        age: 23,
-        location: 'London'
-    };
-    function getLocation(location){
-        if(location){
-            return <p>Location : {location}</p>;
-        }
-    }
-    const templateTwo = (
-        <div>
-            <h4>{user.name ? user.name.toUpperCase() + ' !' : 'Anonymous'}</h4>
-            {(user.age && user.age>=18) && <p>Age: {user.age}</p>}
-            {getLocation(user.location)}
-        </div>
-    );
-
-    const appRoot = document.getElementById('app');
-
-    ReactDOM.render(template, appRoot);
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
